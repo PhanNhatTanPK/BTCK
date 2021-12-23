19eea3388f7bf7c94292f83dd53363ea5f8570ef
annguyenqn <75940633+annguyenqn@users.noreply.github.com>
2021-12-23 15:07:07 +0700

    Add files via upload

---
+++ headerlogin.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=devicqe-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Css/hdlogin.css">
+    <link rel="stylesheet" href="icon/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+</head>
+<body>
+    <?php
+    session_start();
+    include('config.php');
+    ?>
+    <div id="main">
+        <div class="bar2">
+            <div id="header">
+                <div class="logo">
+                    <img height="100%" width="200px" src="pic/Untitled-2 copy.png" />
+                </div>
+                <ul class="nav">
+                    <!-- beggin : nav -->
+                    <li class="tkb"><a href="">Thời Khóa biểu</a></li>
+                    <li>
+                        <a href="">Thành viên </a>
+                        <ul class="subnav">
+                        <li><a href="">An Nguyễn</a></li>
+                        <li><a href="">Tân Phan</a></li>
+                        <li><a href="">Hùng Lê</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Danh sách lớp </a>
+                        <ul class="subnav">
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="http://localhost/Header/plan.php">Kế hoạch </a>
+                        <ul class="subnav">
+                        <li><a href="">Creat</a></li>
+                        <li><a href="">Update</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="login">
+                    <ul class="nav">
+                        <li>
+                            <a href=""><i class="ti-user"></i>
+                                <?php
+
+                                if (!isset($_SESSION['username'])) {
+                                    echo "tai khoan";
+                                } else {
+                                    echo $_SESSION['username'];
+                                }
+                                ?>
+                            </a>
+                            <ul class="subnav">
+                                <li><a href="http://localhost/Header/Login1.php">THÔNG TIN CÁ NHÂN</a></li>
+                                <li><a href="http://localhost/Header/logout.php">ĐĂNG XUẤT</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+       
+
+        <!-- end nav -->
+        <!-- begin : search button -->
+    </div>
+    <?php
+    include('slide.php');
+    ?>
+      
+ 
+</body>
---
+++ slide.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=devicqe-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Css/slide.css">
+    <link rel="stylesheet" href="icon/themify-icons/themify-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="image">
+            <img src="pic/an.jpg" alt="">
+        </div>
+        <div class="image">
+            <img src="pic/hung.jpg" alt="">
+        </div>
+        <div class="image">
+            <img src="pic/tan.jpg" alt="">
+        </div>
+        <div class="image">
+            <img src="pic/an.jpg" alt="">
+        </div>
+        <div class="image">
+            <img src="pic/an.jpg" alt="">
+        </div>
+        <div class="button">
+            <a onclick="nextimg(-1)" class="prev">&#10094;</a>
+            <a onclick="nextimg(1)" class="next">&#10095;</a>
+        </div>
+        <div class="dots">
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+            <span class="dot" onclick="currentSlide(4)"></span>
+            <span class="dot" onclick="currentSlide(5)"></span>
+        </div>
+    </div>
+    <script>
+        var imageno = 1;
+        displayimg(imageno);
+        function nextimg(n) {
+            displayimg(imageno += n)
+        }
+        function currentSlide(n) {
+            displayimg(imageno = n)
+        }
+        function displayimg(n) {
+            var i;
+            var image = document.getElementsByClassName("image");
+            var dots = document.getElementsByClassName("dot");
+            if (n > image.length) {
+                imageno = 1;
+            }
+            if (n < 1) {
+                imageno = image.length;
+            }
+            for (i = 0; i < image.length; i++) {
+                image[i].style.display = "none";
+            }
+              image[imageno - 1].style.display ="block";
+            
+          } 
+
+    </script>
+</body>
