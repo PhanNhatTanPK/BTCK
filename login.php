15e07ddaa84249a92fcaf6f7ec3d614e46de2081
PhanNhatTanPK <PhanNhatTan10062001@gmail.com>
2021-12-29 13:00:36 +0700

    T-update

--- login.php
+++ login.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
+    <link rel="icon" href="./Image/logovuong.png" type="image/x-icon" />
     <title>Đăng nhập</title>
 </head>
 
